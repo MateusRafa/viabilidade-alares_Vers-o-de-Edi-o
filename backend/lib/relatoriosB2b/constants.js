96a8928a9e36108011e8879a1729253fb3a2509f
MateusRafa <medeirosmonte18@gmail.com>
2026-06-03 17:32:40 -0300

    Create constants.js

---
+++ backend/lib/relatoriosB2b/constants.js
@@ -0,0 +1,20 @@
+export const RELATORIOS_B2B_TABLE = 'relatorios_b2b';
+export const RELATORIOS_B2B_BUCKET = 'relatorios-b2b';
+
+export const RELATORIO_STATUS = {
+  EM_ANALISE: 'em_analise',
+  EM_IMPLANTACAO: 'em_implantacao',
+  FINALIZADO: 'finalizado'
+};
+
+export const PAYLOAD_TIPO = {
+  PROJETOS: 'projetos',
+  IMPLANTACAO: 'implantacao'
+};
+
+export const SETOR_ORIGEM = {
+  PROJETOS: 'projetos',
+  IMPLANTACAO: 'implantacao'
+};
+
+export const SIGNED_URL_TTL_SEC = 60 * 60;
