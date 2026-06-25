9c05003782b7555d55c1f7052e9277f7f32de309
MateusRafa <medeirosmonte18@gmail.com>
2026-06-25 15:52:36 -0300

    Update formularioPdfBranding.js

--- frontend/src/tools/formularioPdfBranding.js
+++ frontend/src/tools/formularioPdfBranding.js
@@ -17,12 +17,21 @@ export const BRAND = {
     '/images/assinatura-supervisor.png',
     '/images/assinatura-supervisor.svg'
   ],
+  /** Assinatura na Lista de Material Implantado (Relatório de Construção) */
+  assinaturaImplantacaoSupervisorPaths: [
+    '/images/assinatura-supervisor-implantacao.png',
+    '/images/assinatura-supervisor-implantacao.svg'
+  ],
   /** Pixels RGB acima deste valor viram transparentes (0–255; maior = remove mais cinza) */
   assinaturaFundoClaroLimite: 238,
   /** Nome abaixo da linha de assinatura na Lista de Material */
   assinaturaCoordenadorNome: 'Adônis Pimentel de Araújo',
   /** Cargo abaixo do nome (negrito) na Lista de Material */
   assinaturaCoordenadorCargo: 'Coordenador de Planejamento e Projetos - Engenharia Alares',
+  /** Nome na Lista de Material Implantado */
+  assinaturaImplantacaoCoordenadorNome: 'Jose Rudson Melo Da Silva',
+  /** Cargo na Lista de Material Implantado */
+  assinaturaImplantacaoCoordenadorCargo: 'Coordenador de Implantação',
   /** Nome padrão do campo Coordenador de Projetos (Informações do projeto) */
   coordenadorProjetosNomePadrao: 'Adônis Pimentel de Araújo',
   cores: {
